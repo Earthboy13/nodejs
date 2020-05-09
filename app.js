e529a15118e8f8b80a7e9e964f7857ef41cb9019
Osman Ibrahim <waterboy923@hotmail.com>
2020-05-09 01:00:57 -0400

    404 addded

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    
+    res.status(404).send('<h1>Error 404: Page Not Found!</h1>');
+});
 
 app.listen(8080);
 
