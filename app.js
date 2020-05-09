3fac19735a40c632b2259596c86f0266dce91e48
Osman Ibrahim <waterboy923@hotmail.com>
2020-05-09 02:12:57 -0400

    Styling exported

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const express = require('express'),
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(rootDir, 'public')));
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
