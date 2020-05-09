1fa7aaedfe79012b0fc2ece243a478e2739a684e
Osman Ibrahim <waterboy923@hotmail.com>
2020-05-09 06:54:42 -0400

    EJS used and now kept though i like pug better

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ const express = require('express'),
 // http.createServer(function rqListener(req, res){});
 
 const app = express();
+/* 
 // hbs -> handlebars
 app.engine('hbs', expressHbs({
     layoutsDir: 'views/layouts/', 
     defaultLayout: 'main-layout', 
     extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+})); */
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
